afc47fc3d6c97b9b5a77007a9bc6b8dddff0bd1b
prasad kavinda <pp.kavinda@gmail.com>
2019-03-04 22:08:27 +0530

    add signup with 3 steps

---
+++ app/src/main/java/com/example/newuber/signup/SignupActivity2.java
@@ -0,0 +1,62 @@
+package com.example.newuber.signup;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+
+import com.example.newuber.R;
+
+public class SignupActivity2 extends AppCompatActivity {
+    public static Activity selfActivity;
+    private EditText signupFname, signupLname;
+    private String phoneNo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup2);
+        selfActivity = this;
+        phoneNo = getIntent().getStringExtra("phone");
+
+        signupFname = findViewById(R.id.signup_fname);
+        signupLname = findViewById(R.id.signup_lname);
+
+    }
+
+    public void gotoSignup3 (View view) {
+        signupFname.setError(null);
+        signupLname.setError(null);
+
+        String fname= signupFname.getText().toString();
+        String lname = signupLname.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        if (TextUtils.isEmpty(fname)) {
+            signupFname.setError(getString(R.string.error_field_required));
+            focusView = signupFname;
+            cancel = true;
+        }
+
+        if (TextUtils.isEmpty(lname)) {
+            signupLname.setError(getString(R.string.error_field_required));
+            focusView = signupLname;
+            cancel = true;
+        }
+
+        if (cancel) {
+            focusView.requestFocus();
+        } else {
+            startActivity(new Intent(SignupActivity2.this, SignupActivity3.class)
+            .putExtra("phone", phoneNo)
+            .putExtra("fname", fname)
+            .putExtra("lname", lname));
+        }
+    }
+}
+
