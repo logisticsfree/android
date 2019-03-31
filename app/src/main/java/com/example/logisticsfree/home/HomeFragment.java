5e463beabdd4a87fcac35b291054bb161d096e84
ThusithaGayan <thusitharmtgk@gmail.com>
2019-03-31 21:42:01 +0530

    testing

--- app/src/main/java/com/example/logisticsfree/home/HomeFragment.java
+++ app/src/main/java/com/example/logisticsfree/home/HomeFragment.java
@@ -30,6 +30,7 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
 
+        
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
 }
