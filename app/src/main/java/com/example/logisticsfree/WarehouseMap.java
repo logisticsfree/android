792488b7ef9e707f43217abf1651ec50f24cc779
Prasad Kavinda <pp.kavinda@gmail.com>
2020-10-18 23:02:33 +0530

    Update WarehouseMap.java

--- app/src/main/java/com/example/logisticsfree/WarehouseMap.java
+++ app/src/main/java/com/example/logisticsfree/WarehouseMap.java
@@ -54,7 +54,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
-
 public class WarehouseMap extends AppCompatActivity implements OnMapReadyCallback,
         GoogleMap.OnMyLocationButtonClickListener,
         GoogleMap.OnMyLocationClickListener {
